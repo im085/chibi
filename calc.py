ddb078c9c68093b9bf0931bd3741633bad708eb3
Akiko Morimura <mo8ri5@im085ztto.local>
2019-09-30 16:52:49 +0900

    calc(1*2+3)の途中

--- calc.py
+++ calc.py
@@ -1,5 +1,10 @@
+import re
+
 def calc(s):
-    nums = map(int, s.split("+"))
+    n = 0
+    nums = re.split('\d+', s)
+    symb = re.split('[+*]', s)
+    
     return sum(nums)
 
 print(calc("1+2+3"))
