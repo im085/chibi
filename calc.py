877fee306b14c7734b38d92eb3710027265b6fc8
Akiko Morimura <mo8ri5@im085ztto.local>
2019-10-01 09:51:43 +0900

    4の途中、リストまでは作れたけどif文が未完成

--- calc.py
+++ calc.py
@@ -1,10 +1,25 @@
 import re
 
 def calc(s):
-    n = 0
-    nums = re.split('\d+', s)
-    symb = re.split('[+*]', s)
-    
-    return sum(nums)
+    symb = re.split('\d+', s)
+    nums = re.split('[+*]', s)
+    nums = [int(j) for j in nums]
 
-print(calc("1+2+3"))
+    print(nums)
+
+    n = nums[0]
+    k = 1
+
+    for i in symb:
+        if i == "+":
+            n = n + nums[k]
+            k = k + 1
+        elif i == "*":
+            n = n * nums[k]
+            k = k + 1
+        else:
+            pass
+ 
+    return n
+
+print(calc("1+2+3*4"))
