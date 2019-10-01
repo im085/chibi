59b14a175389704f97b4420db6394138bb0851d5
Akiko Morimura <mo8ri5@im085ztto.local>
2019-10-01 10:24:50 +0900

    calc

--- calc.py
+++ calc.py
@@ -2,19 +2,34 @@ import re
 
 def calc(s):
     symb = re.split('\d+', s)
-    nums = re.split('[+*]', s)
-    nums = [int(j) for j in nums]
+    nums = re.split('[+*-/]', s)
+    nums = [float(j) for j in nums]
     del symb[0]
     del symb[-1]
-    i = 0
 
+    i = 0
     for j in range(len(symb)):
         if symb[i] == "*":
            nums[i] = nums[i] * nums[i+1]
            del nums[i+1]
            del symb[i]
            i = i - 1
+        elif symb[i] == "/":
+           nums[i] = nums[i] / nums[i+1]
+           del nums[i+1]
+           del symb[i]
+           i = i - 1
         i = i + 1
+    
+    i = 0
+    for j in range(len(symb)):
+        if symb[i] == "-":
+           nums[i] = nums[i] - nums[i+1]
+           del nums[i+1]
+           del symb[i]
+           i = i - 1
+        i = i + 1
+    
     return sum(nums)
 
-print(calc("3+7*4"))
+print(calc("4*7/2+5-1"))
