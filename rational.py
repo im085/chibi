9c9a16b245466f0a5728ab6e4e1b4355c51be919
Akiko Morimura <mo8ri5@im085ztto.local>
2019-10-07 16:21:19 +0900

    added "+"

--- rational.py
+++ rational.py
@@ -3,10 +3,18 @@ class Q(object):
         self.a = a
         self.b = b
 
+    def add(self, q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d+b*c, b*d)
+
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
         else:
             return f"{self.a}/{self.b}"
-q = Q(3)
-print(q)
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1.add(q2))
