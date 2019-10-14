e1ecf5644db72b33ce2703510a76600a2d46a2f7
Akiko Morimura <mo8ri5@im085ztto.local>
2019-10-14 16:26:52 +0900

    コメントアウトも効かないんだけど

--- exp.py
+++ exp.py
@@ -1,13 +1,12 @@
 class Expr(object):
     pass
 
-
 class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
     def __repr__(self):
-        return 'Val(1)' #どう頑張ってもここエラーになるので諦めた
+        return f'Val({self.value})'
     def eval(self):
         return self.value
 
@@ -15,20 +14,24 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
-assert isinstance(v, Expr)
-assert isinstance(v, Val)
-
+assert isinstance(v, Expr) # ==> True
+assert isinstance(v, Val) # ==> True
+assert not isinstance(v, int) 
 
 class Add(Expr):
-    __slots__ = ['left', 'right']
+    __slots__=['left', 'right']
     def __init__(self, a, b):
-        self.left = a
+        self.left = a   # aとb は式
         self.right = b
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-e = Add(Val(1), Val(2))
+e = Add(Val(1), Val(2))  # 1+2
 assert e.eval() == 3
 
-e = Add(Val(1), Add(Val(2), Val(3)))
+e = Add(1,2)
+assert e.eval() == 3
+
+
+e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
