07571b268c023eec4e18310c654dea5d7050d010
Akiko Morimura <mo8ri5@im085ztto.local>
2019-10-28 16:51:48 +0900

    とりあ

--- parser.py
+++ parser.py
@@ -1,8 +1,35 @@
-from exp import  Val, Add
+from exp import Val, Add, Sub, Mul, Div
 
-def parse(s: str):
+'''
+def parse(s:str):
     num = int(s)
     return Val(num)
 
 e = parse("123")
 print(e)
+
+s = "123+456"
+pos =s.find('+')
+print('pos', pos)
+
+s1 = s[0:pos]
+s2 = s[pos+1:]
+print(s, s1, s2)
+'''
+
+def parse(s: str):
+    if s.find('+') > 0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
+
+
+e = parse("1+2+3")
+print(e, e.eval())
