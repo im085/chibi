f632c59cc92aea3440afd22b69e3b531cbf257cc
Akiko Morimura <mo8ri5@im085ztto.local>
2019-11-25 16:43:42 +0900

    打ち終わった

--- icalc.py
+++ icalc.py
@@ -1,17 +1,68 @@
 import pegpy
 
-peg = pegpy.grammar("""
-Expression = Product (^{ '+' Product #Add })*
-Product = Value (^{ '*' Value #Mul })*
-Value = { [0-9]+ #Int }
-""")
+#peg = pegpy.grammar("""
+#Expression = Product (^{ '+' Product #Add })*
+#Product = Value (^{ '*' Value #Mul })*
+#Value = { [0-9]+ #Int }
+#""")
+peg = pegpy.grammar("chibi.tpeg")
 parser = pegpy.generate(peg)
+
+class Expr( object ):
+    @classmethod
+    def new(cls , v):
+        if isinstance (v, Expr):
+            return v
+        return Val(v)
+
+class Val(Expr):
+    __slot__ = ["value "]
+    def __init__(self , value):
+        self.value = value
+    def __repr__(self):
+        return f"Val ({ self . value }) "
+    def eval (self , env: dict ):
+        return self.value
+
+class Binary(Expr):
+    __slot__ = ["left ", "right "]
+    def __init__(self , left , right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f"{ classname }({ self . left },{ self . right }) "
+
+class Add(Binary):
+    __slot__ = ["left ", "right "]
+    def eval (self , env: dict ):
+        return self.left. eval (env) + self.right. eval (env)
+
+class Sub(Binary):
+    __slot__ = ["left ", "right "]
+    def eval (self , env: dict ):
+        return self.left. eval (env) - self.right. eval (env)
+
+class Mul(Binary):
+    __slot__ = ["left ", "right "]
+    def eval (self , env: dict ):
+        return self.left. eval (env) * self.right. eval (env)
+
+class Div(Binary):
+    __slot__ = ["left ", "right "]
+    def eval (self , env: dict ):
+        return self.left. eval (env) // self.right. eval (env)
+
+class Mod(Binary):
+    __slot__ = ["left ", "right "]
+    def eval (self , env: dict ):
+        return self.left. eval (env) % self.right. eval (env)
 
 """t = parser("1+2*3")
 print(repr(t))
 
 t = parser("@2")
-print(repr(t))"""
+print(repr(t))
 
 def calc(t):
     if t == "Int":
@@ -23,9 +74,9 @@ def calc(t):
     print(f"TODO {t.tag}")
     return 0
 
-"""t = parser("1+2*3+4*5")
+t = parser("1+2*3+4*5")
 print(repr(t))
-print(calc(t))"""
+print(calc(t))
 
 def main():
     while(True):
@@ -33,7 +84,36 @@ def main():
         if (s == "end"):
             break
         t = parser(s)
-        print(calc(t))
+        print(calc(t))"""
+
+def conv(tree):
+    if tree == "Block":
+        return conv(tree[0])
+    if tree == "Val" or tree == "Int":
+        return Val(int(str(tree)))
+    if tree == "Add":
+        return Add(conv(tree[0]), conv(tree[1]))
+    print("@TODO", tree.tag)
+    return Val(str(tree))
+
+def run(src: str):
+    tree = parser(src)
+    if tree.isError():
+        print(repr(tree))
+    else:
+        e = conv(tree)
+        print(repr(e))
+        print(e.eval({}))
+
+def main():
+    try:
+        while True:
+            s = input(">>> ")
+            if s == "":
+                break
+            run(s)
+    except EOFError:
+        return
 
 if __name__ == "__main__":
     main()
