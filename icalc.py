2ce64984b1b3c8620946a061bf2a6d24594267e8
Akiko Morimura <mo8ri5@im085ztto.local>
2019-11-25 16:49:42 +0900

    今日の最終変更

--- icalc.py
+++ icalc.py
@@ -8,7 +8,7 @@ import pegpy
 peg = pegpy.grammar("chibi.tpeg")
 parser = pegpy.generate(peg)
 
-class Expr( object ):
+class Expr(object):
     @classmethod
     def new(cls , v):
         if isinstance (v, Expr):
@@ -17,44 +17,57 @@ class Expr( object ):
 
 class Val(Expr):
     __slot__ = ["value "]
+
     def __init__(self , value):
         self.value = value
+
     def __repr__(self):
         return f"Val ({ self . value }) "
+
     def eval (self , env: dict ):
         return self.value
 
+e = Val(0)
+assert e.eval({}) == 0
+
 class Binary(Expr):
     __slot__ = ["left ", "right "]
+
     def __init__(self , left , right):
         self.left = Expr.new(left)
         self.right = Expr.new(right)
+
     def __repr__(self):
         classname = self.__class__.__name__
         return f"{ classname }({ self . left },{ self . right }) "
 
 class Add(Binary):
     __slot__ = ["left ", "right "]
+
     def eval (self , env: dict ):
         return self.left. eval (env) + self.right. eval (env)
 
 class Sub(Binary):
     __slot__ = ["left ", "right "]
+
     def eval (self , env: dict ):
         return self.left. eval (env) - self.right. eval (env)
 
 class Mul(Binary):
     __slot__ = ["left ", "right "]
+
     def eval (self , env: dict ):
         return self.left. eval (env) * self.right. eval (env)
 
 class Div(Binary):
     __slot__ = ["left ", "right "]
+
     def eval (self , env: dict ):
         return self.left. eval (env) // self.right. eval (env)
 
 class Mod(Binary):
     __slot__ = ["left ", "right "]
+    
     def eval (self , env: dict ):
         return self.left. eval (env) % self.right. eval (env)
 
